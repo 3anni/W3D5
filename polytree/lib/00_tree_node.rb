d722cb8ecb2e2ab1d0a627dc735a6180949179d6
Will Bannister <bannister.will@gmail.com>
2022-10-21 14:38:56 -0700

    completed polytree

--- polytree/lib/00_tree_node.rb
+++ polytree/lib/00_tree_node.rb
@@ -1,7 +1,10 @@
-class PolyTreeNode
-    attr_reader :value
 
-    attr_accessor :children, :parent
+require 'byebug'
+
+class PolyTreeNode
+    attr_reader :value, :parent
+
+    attr_accessor :children
 
     def initialize(value)
         @value = value
@@ -9,28 +12,22 @@ class PolyTreeNode
         @children = []
     end
 
-    def parent= (parent)
-        @parent = parent
-        return nil if parent.nil?
-        parent.add_child(self)
+    def parent= (new_parent)
+        # remove child from current parent
+        self.parent.children.delete(self) unless self.parent.nil?
+        # set new parent
+        @parent = new_parent
+        # add child to new parent
+        self.parent.children << self unless self.parent.nil?
     end
 
     def add_child(child)
-        # parent
-        # child.parent=(@parent)
-        @children << child if !@children.include?(child)
+        child.parent=(self) if !children.include?(child)
     end
 
     def remove_child(child)
-        #pass nil to child's parent=
-        #raise error if not child
-        if child.parent == nil
-            raise "not child "
-        end
-        child.parent = nil
-        @children.delete(child)
+        raise "not child " if child.parent == nil
 
+        child.parent = nil if children.include?(child)
     end
-
-
 end
