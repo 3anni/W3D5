7f5049284897a71968c0764d5130c6755dcfbf83
Will Bannister <bannister.will@gmail.com>
2022-10-21 12:04:57 -0700

    added early functions

--- polytree/lib/00_tree_node.rb
+++ polytree/lib/00_tree_node.rb
@@ -1,3 +1,28 @@
 class PolyTreeNode
+    attr_reader :value, :parent
+
+    attr_accessor :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent= (parent)
+        @parent = parent
+        parent.add_child(self)
+    end
+
+    def add_child(child)
+        # parent
+        # child.parent=(@parent)
+        @children << child if !@children.include?(child)
+    end
+
+    def remove_child(child)
+
+    end
+
 
 end
